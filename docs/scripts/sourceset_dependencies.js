aa034fb6bb1b536bcd563e3e83d5eddae2ef36ab
Will Van De Camp <will@ubipark.com>
2022-10-03 22:29:06 +1100

    More android 12 Updates. Update dokka. v1.0.21

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":UbiParkSDK:dokkaHtml/androidTestRelease":[],":UbiParkSDK:dokkaHtml/debug":[],":UbiParkSDK:dokkaHtml/main":[],":UbiParkSDK:dokkaHtml/release":[]}'
+sourceset_dependencies='{":UbiParkSDK:dokkaHtml/androidTestRelease":[],":UbiParkSDK:dokkaHtml/debug":[],":UbiParkSDK:dokkaHtml/main":[],":UbiParkSDK:dokkaHtml/release":[],":UbiParkSDK:dokkaHtml/testFixtures":[],":UbiParkSDK:dokkaHtml/testFixturesDebug":[],":UbiParkSDK:dokkaHtml/testFixturesRelease":[]}'
